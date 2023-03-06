9a7096ed3a708c04a14aedc048dee5e6ba1aaf2e
Taha Rawjani <tahakazim08@gmail.com>
2023-03-06 07:52:43 -0500

    idk

--- src/game.asm
+++ src/game.asm
@@ -1,110 +1,43 @@
 [org 0x7C00]
 [bits 16]
 
+nop
+jmp short _start
+
+%include "src/util.asm"
+%include "src/draw_player.asm"
+
 _start:
     mov bp, 0x7C00
     mov sp, bp
 
-    call clear_screen
-
-    mov dx, 1
-    mov si, 30
-
-loop:
+.loop:
 
     call clear_screen
-    call draw_plane
-    call get_char
-
-    cmp al, byte 's'
-
-    je move_down
-
-    cmp al, byte 'w'
-
-    je move_up
-
-    jmp loop
-
-
-move_up:
-
-    cmp dx, 1
-    jl loop
-
-    sub si, 5
-    sub dx, 5
-    jmp loop
-
-move_down:
-    cmp si, 199
-    jg loop
-
-    add si, 5
-    add dx, 5
-    jmp loop
-
-
-;DX = starting y
-
-draw_plane:
-
-    push ax
-    push bx
-    push cx
-    push dx
-    mov al, 15
-    mov ah, 0x0C
-    mov bh, 0
-
-.loop_y:
-
-    mov cx, 0
-
-.loop_x:
     
-    int 0x10
 
-    cmp cx, 2
+    jmp .loop
 
-    jge .loop_y_end
+input_char:
+    db 0
 
-    inc cx
+p1_x_pos:
+    dw 0
 
-    jmp .loop_x
+p1_y_pos:
+    dw 0
 
-.loop_y_end:
+p2_x_pos:
+    dw 0
 
-    cmp dx, si
+p2_y_pos:
+    dw 0
 
-    jge .end
+ball_x_pos:
+    dw 0
 
-    inc dx
-
-    jmp .loop_y    
-
-.end:
-
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    ret
-
-;AL = CHAR
-
-get_char:
-    mov ah, 0x00
-    int 0x16
-    ret
-
-clear_screen:
-    push ax
-    mov ah, 0x00
-    mov al, 0x13
-    int 0x10
-    pop ax
-    ret
+ball_y_pos:
+    dw 0
 
 times 510-($-$$) db 0
 dw 0xAA55
